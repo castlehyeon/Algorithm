400a2b5af49567aac23aada9e849997801e65438
Sapjil <53210680+castlehyeon@users.noreply.github.com>
2023-04-26 17:05:19 +0900

    [Bronze V] Title: 개, Time: 128 ms, Memory: 14132 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,14 @@
+class Main {
+    public static void main(String[] args){
+
+
+
+
+
+        System.out.printf("|\\_/|\n");
+        System.out.printf("|q p|   /}\n");
+        System.out.printf("( 0 )\"\"\"\\\n");
+        System.out.printf("|\"^\"`    |\n");
+        System.out.printf("||_/=\\\\__|\n");
+    }
+}
