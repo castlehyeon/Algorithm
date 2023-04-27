2cb395e92731ca3660a23f53136580219a024582
Sapjil <53210680+castlehyeon@users.noreply.github.com>
2023-04-28 05:16:49 +0900

    [Bronze V] Title: 윤년, Time: 124 ms, Memory: 14292 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.time.LocalDate;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//        String[] s = br.readLine().split(" ");
+
+        int a = Integer.parseInt(br.readLine());
+//        int b = Integer.parseInt(br.readLine());
+        if(a%4==0&&a%100!=0||a%400==0){
+            bw.write("1");
+//        } else if (a<0&&b>0) {
+//            bw.write("2");
+//        }else if (a>0&&b<0) {
+//            bw.write("4");
+//        }else if (a>0&&b>0) {
+//            bw.write("1");
+        }else {
+            bw.write("0");
+        }
+        bw.flush();
+        bw.close();
+
+    }
+}
