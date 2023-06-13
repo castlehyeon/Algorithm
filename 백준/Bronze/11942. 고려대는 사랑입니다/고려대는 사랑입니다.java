82b1a134269f4899abf79dddc6beaf975b9839a5
Sapjil <53210680+castlehyeon@users.noreply.github.com>
2023-06-13 17:47:54 +0900

    [Bronze V] Title: 고려대는 사랑입니다, Time: 196 ms, Memory: 17196 KB -BaekjoonHub

---
+++ 백준/Bronze/11942. 고려대는 사랑입니다/고려대는 사랑입니다.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        Scanner sc = new Scanner(System.in);
+
+        bw.write("고려대학교");
+
+        br.close();
+        bw.close();
+
+    }
+}
