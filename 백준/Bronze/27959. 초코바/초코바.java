9ffb5a09221490d7f18859754ff7aa5ddd2d14de
Sapjil <53210680+castlehyeon@users.noreply.github.com>
2023-08-01 20:55:04 +0900

    [Bronze V] Title: 초코바, Time: 124 ms, Memory: 14340 KB -BaekjoonHub

---
+++ 백준/Bronze/27959. 초코바/초코바.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.*;
+
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] s = br.readLine().split(" ");
+        int N = Integer.parseInt(s[0]);
+        int M = Integer.parseInt(s[1]);
+
+        if (100 * N < M) {
+            bw.write("No");
+        } else bw.write("Yes");
+
+        br.close();
+        bw.close();
+    }
+}
